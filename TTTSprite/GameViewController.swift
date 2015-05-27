aa281d140e1ae7be5869c071fef8d53ace54fc43
can1004 <akyurekm7@gmail.com>
2015-05-27 15:47:28 +0300

    jeans update
    ben de deneyeyim mi accık.
    didReceiveMemoryWarning() bu oyunda kullanılmayacak bi fonksiyon,
    kalabalık etmesin diye kaldırdım

--- TTTSprite/GameViewController.swift
+++ TTTSprite/GameViewController.swift
@@ -44,6 +44,7 @@ class GameViewController: UIViewController {
         }
     }
 
+
     override func shouldAutorotate() -> Bool {
         return true
     }
@@ -56,10 +57,6 @@ class GameViewController: UIViewController {
         }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
 
     override func prefersStatusBarHidden() -> Bool {
         return true
